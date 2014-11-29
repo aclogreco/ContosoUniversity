cd501ee4162e7e211bf967e25ed80c05333aacc1
Anthony LoGreco <aclogreco@gmail.com>
2014-11-29 04:12:34 -0500

    Update Delete GET and POST methods in the Student Controller. Add an error message place holder to the Student Delete View.

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -14,12 +14,12 @@ namespace ContosoUniversity.Controllers {
 		private SchoolContext db = new SchoolContext();
 
 		// GET: Student
-		public ActionResult Index () {
+		public ActionResult Index() {
 			return View(db.Students.ToList());
 		}
 
 		// GET: Student/Details/5
-		public ActionResult Details (int? id) {
+		public ActionResult Details(int? id) {
 			if (id == null) {
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 			}
@@ -31,7 +31,7 @@ namespace ContosoUniversity.Controllers {
 		}
 
 		// GET: Student/Create
-		public ActionResult Create () {
+		public ActionResult Create() {
 			return View();
 		}
 
@@ -40,7 +40,7 @@ namespace ContosoUniversity.Controllers {
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult Create ([Bind(Include = "LastName,FirstMidName,EnrollmentDate")] Student student) {
+		public ActionResult Create([Bind(Include = "LastName,FirstMidName,EnrollmentDate")] Student student) {
 			try {
 
 
@@ -58,7 +58,7 @@ namespace ContosoUniversity.Controllers {
 		}
 
 		// GET: Student/Edit/5
-		public ActionResult Edit (int? id) {
+		public ActionResult Edit(int? id) {
 			if (id == null) {
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 			}
@@ -98,9 +98,12 @@ namespace ContosoUniversity.Controllers {
 		}
 
 		// GET: Student/Delete/5
-		public ActionResult Delete (int? id) {
+		public ActionResult Delete(int? id, bool? saveChangesError = false) {
 			if (id == null) {
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			if (saveChangesError.GetValueOrDefault()) {
+				ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
 			}
 			Student student = db.Students.Find(id);
 			if (student == null) {
@@ -110,16 +113,23 @@ namespace ContosoUniversity.Controllers {
 		}
 
 		// POST: Student/Delete/5
-		[HttpPost, ActionName("Delete")]
+		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult DeleteConfirmed (int id) {
-			Student student = db.Students.Find(id);
-			db.Students.Remove(student);
-			db.SaveChanges();
+		public ActionResult Delete(int id) {
+			try {
+				Student student = db.Students.Find(id);
+				db.Students.Remove(student);
+				db.SaveChanges();
+			}
+			catch (DataException /* dex */) {
+				// Log the error (uncomment dex variable name and add a line here to write a log.)
+				return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+			}
+
 			return RedirectToAction("Index");
 		}
 
-		protected override void Dispose (bool disposing) {
+		protected override void Dispose(bool disposing) {
 			if (disposing) {
 				db.Dispose();
 			}
