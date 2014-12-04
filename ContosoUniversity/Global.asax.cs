46390b9385ce2192a491a078e25b735a30046932
Anthony LoGreco <aclogreco@gmail.com>
2014-12-04 05:39:13 -0500

    Enable the interceptor classes to be run when the Entity Framework sends queries to the database.

--- ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ContosoUniversity.DAL;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace ContosoUniversity {
 	public class MvcApplication : System.Web.HttpApplication {
@@ -13,6 +15,8 @@ namespace ContosoUniversity {
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			DbInterception.Add(new SchoolInterceptorTransientErrors());
+			DbInterception.Add(new SchoolInterceptorLogging());
 		}
 	}
 }
